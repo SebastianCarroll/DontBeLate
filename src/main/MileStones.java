74a661b057ef795f188ed2aca8369689d0aaaa33
SebastianCarroll <seabass25@hotmail.com>
2014-03-10 12:11:34 +1100

    Cleaned up file

--- src/main/MileStones.java
+++ src/main/MileStones.java
@@ -1,10 +1,10 @@
 package main;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
-
 
 //import javax.faces.bean.*;
 import javax.inject.Named;
@@ -13,44 +13,35 @@ import javax.enterprise.context.SessionScoped;
 @Named("milestones")
 @SessionScoped
 public class MileStones implements Serializable {
-	
+
 	private Calendar end = new GregorianCalendar();
-	private ArrayList<MileStone> milestones = 
-			new ArrayList<MileStone>();
-	
+	private ArrayList<MileStone> milestones = new ArrayList<MileStone>();
+
 	/**
 	 * Default constructor: Necessary as jsf requires it
 	 */
-	public MileStones(){
+	public MileStones() {
 		this.setEnd(new GregorianCalendar());
-	}
-
-	public String getMilestones() {
-		return Formatter.mileStonesToText(milestones);
-	}
-
-	public void setMilestones(String textList) {
-		this.milestones = Formatter.textToMileStones(textList);
 	}
 
 	public MileStones(Calendar end2) {
 		this.setEnd(end2);
 	}
 
+	public void addMileStone(MileStone milestone) {
+		milestones.add(milestone);
+	}
+
+	public void addNewMilestone() {
+		milestones.add(new MileStone("", 0));
+	}
+
 	public Calendar getEnd() {
 		return end;
 	}
 
-	public void setEnd(Calendar end) {
-		this.end = end;
-	}
-	
-	public void addMileStone(MileStone milestone){
-		milestones.add(milestone);
-	}
-	
-	public void addNewMilestone(){
-		milestones.add(new MileStone("", 0));
+	public String getMilestones() {
+		return Formatter.mileStonesToText(milestones);
 	}
 
 	public Calendar getStart() {
@@ -60,25 +51,33 @@ public class MileStones implements Serializable {
 		start.add(Calendar.MINUTE, -duration);
 		return start;
 	}
-	
-	@Override
-	public String toString(){
-		SimpleDateFormat df = new SimpleDateFormat("HH:mm");
-		String output = df.format(end.getTime()) + " - End\n";
-		Calendar current = (Calendar) end.clone();
-		for(MileStone m : milestones){
-			current.add(Calendar.MINUTE, -m.getDuration());
-			output += df.format(current.getTime()) + 
-					" - " + m.getDescription() + "\n";
-		}
-		return output;
+
+	public void setEnd(Calendar end) {
+		this.end = end;
+	}
+
+	public void setMilestones(String textList) {
+		this.milestones = Formatter.textToMileStones(textList);
 	}
 
 	private int sumDuration() {
 		int duration = 0;
-		for(MileStone m : milestones){
+		for (MileStone m : milestones) {
 			duration += m.getDuration();
 		}
 		return duration;
+	}
+
+	@Override
+	public String toString() {
+		SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+		String output = df.format(end.getTime()) + " - End\n";
+		Calendar current = (Calendar) end.clone();
+		for (MileStone m : milestones) {
+			current.add(Calendar.MINUTE, -m.getDuration());
+			output += df.format(current.getTime()) + " - " + m.getDescription()
+					+ "\n";
+		}
+		return output;
 	}
 }
